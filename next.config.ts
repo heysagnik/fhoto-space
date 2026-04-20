ee5f80ff06bc10cac702615a24873e171e2c337a
Sagnik Sahoo <sahoosagnik1@gmail.com>
2026-04-21 01:27:27 +0530

    chore: comment out unoptimized image configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.137.1'],
   images: {
-    unoptimized: true,
+    // unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
